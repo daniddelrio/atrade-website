1d1097174c0824f560a35aa41069d8178e1bd5e7
Riana Lim <limriana9@gmail.com>
2019-10-24 19:46:06 +0800

    update settings.py to include packages

--- ateneo_trade/ateneo_trade/settings.py
+++ ateneo_trade/ateneo_trade/settings.py
@@ -26,6 +26,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+	‘home’, 
+	‘social_django’,
 ]
 
 MIDDLEWARE = [
@@ -107,3 +109,47 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Authentication
+
+AUTHENTICATION_BACKENDS = (
+ ‘social_core.backends.open_id.OpenIdAuth’,
+ ‘social_core.backends.google.GoogleOpenId’,
+ ‘social_core.backends.google.GoogleOAuth2’,
+ ‘django.contrib.auth.backends.ModelBackend’,
+)
+
+SOCIAL_AUTH_URL_NAMESPACE = ‘social’
+'context_processors': [
+	'social_django.context_processors.backends',
+	'social_django.context_processors.login_redirect',
+],
+
+SOCIAL_AUTH_PIPELINE = (
+	'social.pipeline.social_auth.social_details'.
+	'social.pipeline.social_auth.social_uid',
+	'social.pipeline.social_auth.auth_allowed',
+	'social.pipeline.social_auth.social_user',
+	'social.pipeline.user.get_username',
+	'social.pipeline.user.create_user',
+	'social.pipeline.social_auth.associate_user',
+	'social.pipeline.debug.debug',
+	'social.pipeline.social_auth.load_extra_data',
+	'social.pipeline.user.user_details',
+	'social.pipeline.debug.debug',
+)
+
+SOCIAL_AUTH_GOOGLE_OAUTH2_IGNORE_DEFAULT_SCOPE = True
+SOCIAL_AUTH_GOOGLE_OAUTH2_SCOPE = [
+	'https://www.googleapis.com/auth/userinfo.email',
+	'https://www.googleapis.com/auth/userinfo.profile'
+]
+# Google+ SignIn (google-plus)
+SOCIAL_AUTH_GOOGLE_PLUS_IGNORE_DEFAULT_SCOPE = True
+SOCIAL_AUTH_GOOGLE_PLUS_SCOPE = [
+'https://www.googleapis.com/auth/plus.login',
+'https://www.googleapis.com/auth/userinfo.email',
+'https://www.googleapis.com/auth/userinfo.profile'
+]
+
+LOGIN_URL = '/account/login/'
