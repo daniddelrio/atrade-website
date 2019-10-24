6af753b470ae1eb37d8d4d7e9c21f17791d209c9
Riana Lim <limriana9@gmail.com>
2019-10-24 20:13:45 +0800

    added routes to views

--- ateneo_trade/shop/urls.py
+++ ateneo_trade/shop/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path, include
+from django.conf.urls import url
+from . import views
 
 urlpatterns = [
+    url(r'^$', views.Home),
+    url(r'^profile/$', views.update_profile),
+    url(r'^account/logout/$', views.Logout),
     # path('', include('shop.urls'))
 ]
