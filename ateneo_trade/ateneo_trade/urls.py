b1467c95f4a994914a97fc5e5f9d357eb56dda45
Riana Lim <limriana9@gmail.com>
2019-10-24 20:06:14 +0800

    add routes to urls.py

--- ateneo_trade/ateneo_trade/urls.py
+++ ateneo_trade/ateneo_trade/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('shop.urls'))
+	path('admin/', admin.site.urls),
+	path('', include('shop.urls'))
+	# url(r'^', include('home.urls')),
+	url(r'^account/', include('social_django.urls', namespace='social')),
+	url(r'^account/', include('django.contrib.auth.urls', namespace='auth')),
 ]
