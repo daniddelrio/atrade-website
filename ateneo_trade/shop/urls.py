13ca24788f3b9e886178628af6d8e4acdddd9127
Riana Lim <limriana9@gmail.com>
2019-10-25 14:37:48 +0800

    add stuff to view and url

--- ateneo_trade/shop/urls.py
+++ ateneo_trade/shop/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path, include
-from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.Home),
-    url(r'^profile/$', views.update_profile),
-    url(r'^account/logout/$', views.Logout),
-    # path('', include('shop.urls'))
+    path('', views.Home),
+    path('profile/', views.update_profile),
+    path('account/logout/', views.Logout),
 ]
