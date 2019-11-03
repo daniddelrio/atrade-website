161c3ac9356eb867c176fccde9cedd3ea30cd335
Riana Lim <limriana9@gmail.com>
2019-11-03 22:52:47 +0800

    updated profile table

--- ateneo_trade/shop/models.py
+++ ateneo_trade/shop/models.py
@@ -5,9 +5,12 @@ from django.dispatch import receiver
 
 class Profile(models.Model):
 	user = models.OneToOneField(User,unique=True, null=False, db_index=True, on_delete=models.CASCADE)
-	bio = models.TextField(max_length=500, blank=True)
-	location = models.CharField(max_length=30, blank=True)
-	birth_date = models.DateField(null=True, blank=True)
+	school = models.TextField(max_length=4,blank=True)
+	grad_year = models.IntegerField(validators=[alidators=[MinValueValidator(1859), MaxValueValidator(9999)]], default_value=current_year,blank=True)
+	course = models.IntegerField(max_length=10,null=False,blank=True,default_value="")
+	trade_pts = models.IntegerField();
+	contact_num = models.IntegerField(null=False,blank=False);
+	fb_link = models.CharField(null=False,blank=False);
 	
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
@@ -17,3 +20,6 @@ def create_user_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
 	instance.profile.save()
+
+def current_year():
+    return datetime.date.today().year
