34a581abe9daf9d71a126e7125d89362a1837a61
Riana Lim <limriana9@gmail.com>
2019-11-03 22:24:40 +0800

    switched to mysql

--- ateneo_trade/ateneo_trade/settings.py
+++ ateneo_trade/ateneo_trade/settings.py
@@ -67,8 +67,12 @@ WSGI_APPLICATION = 'ateneo_trade.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'atrade_db',
+		'USER': 'root',
+		'PASSWORD': '',
+		'HOST': '',
+		'PORT': '',
     }
 }
 
