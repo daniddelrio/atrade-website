811fdc571737c195bdaafb33abc996aae3ccfc2c
Riana Lim <limriana9@gmail.com>
2019-11-03 23:45:23 +0800

    fix tabs

--- ateneo_trade/shop/views.py
+++ ateneo_trade/shop/views.py
@@ -6,32 +6,32 @@ from django.contrib.auth import logout
 from django.http import HttpResponseRedirect
 from django.db import transaction
 from .models import Profile
-from .forms import UserForm,ProfileForm
+from .forms import UserForm, ProfileForm
 
 @login_required
 def Home(request):
-    return render(request, 'shop/home.html')
+	return render(request, 'shop/home.html')
 
 @login_required
 @transaction.atomic
 def update_profile(request):
-    if request.method == 'POST':
-        user_form = UserForm(request.POST, instance=request.user)
-        profile_form = ProfileForm(request.POST, instance=request.user.profile)
-        if user_form.is_valid() and profile_form.is_valid():
-            user_form.save()
-            profile_form.save()
-            return HttpResponseRedirect('/')
-        else:
-            messages.error(request, _('Please correct the error below.'))
-    else:
-        user_form = UserForm(instance=request.user)
-        profile_form = ProfileForm(instance=request.user.profile)
-    return render(request, 'shop/profile.html', {
-        'user_form': user_form,
-        'profile_form': profile_form
-    })
+	if request.method == 'POST':
+		user_form = UserForm(request.POST, instance=request.user)
+		profile_form = ProfileForm(request.POST, instance=request.user.profile)
+		if user_form.is_valid() and profile_form.is_valid():
+			user_form.save()
+			profile_form.save()
+			return HttpResponseRedirect('/')
+		else:
+			messages.error(request, _('Please correct the error below.'))
+	else:
+		user_form = UserForm(instance=request.user)
+		profile_form = ProfileForm(instance=request.user.profile)
+	return render(request, 'shop/profile.html', {
+		'user_form': user_form,
+		'profile_form': profile_form
+	})
 
 def Logout(request):
-    logout(request)
-    return HttpResponseRedirect('/')
+	logout(request)
+	return HttpResponseRedirect('/')
