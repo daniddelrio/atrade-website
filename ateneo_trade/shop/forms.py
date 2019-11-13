df0a69c7dec6c7d7e2c1d3dfe6736f5b0f406122
Riana Lim <limriana9@gmail.com>
2019-11-13 23:41:54 +0800

    update forms.py

--- ateneo_trade/shop/forms.py
+++ ateneo_trade/shop/forms.py
@@ -5,16 +5,16 @@ from shop.models import Profile, Item
 from django import forms
 
 class UserForm(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = ('first_name', 'last_name')
+	class Meta:
+		model = User
+		fields = ('first_name', 'last_name')
 
 class ProfileForm(forms.ModelForm):
-    class Meta:
-        model = Profile
-        fields = ('school','major','grad_year','contact_num','fb_link')
+	class Meta:
+		model = Profile
+		fields = ('school','major','grad_year','contact_num','fb_link')
 
 class ItemForm(forms.ModelForm):
-    class Meta:
-        model = Item
-        fields = ('price','description','category','location')
+	class Meta:
+		model = Item
+		fields = ('price','description','category','location')
