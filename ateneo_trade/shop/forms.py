02186be01c14ac1eb63709b530f1a92c6323da06
Riana Lim <limriana9@gmail.com>
2019-11-13 23:35:12 +0800

    created item form

--- ateneo_trade/shop/forms.py
+++ ateneo_trade/shop/forms.py
@@ -1,14 +1,20 @@
 from django.shortcuts import redirect, HttpResponseRedirect
 from django.contrib.auth import logout
 from django.contrib.auth.models import User
-from shop.models import Profile
+from shop.models import Profile, Item
 from django import forms
 
 class UserForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ('first_name', 'last_name', 'email')
+        fields = ('first_name', 'last_name')
+
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ('school','major','grad_year','contact_num','fb_link')
+
+class ItemForm(forms.ModelForm):
+    class Meta:
+        model = Item
+        fields = ('price','description','category','location')
