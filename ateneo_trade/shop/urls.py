a4d6f32dbea530f1f58e4a2ac8f90864fb8bd33a
Riana Lim <limriana9@gmail.com>
2019-11-13 23:42:30 +0800

    add post item to urls.py

--- ateneo_trade/shop/urls.py
+++ ateneo_trade/shop/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.Home, name='home'),
     path('profile/', views.update_profile, name='profile'),
+    path('post/', views.post_item, name='post_item'),
     path('account/logout/', views.Logout, name='logout'),
 ]
