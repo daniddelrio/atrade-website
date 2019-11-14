a776e3156c322c5892cf411aff570253a54f4c2f
Riana Lim <limriana9@gmail.com>
2019-11-14 13:12:05 +0800

    fixed bug in forms.py

--- ateneo_trade/shop/forms.py
+++ ateneo_trade/shop/forms.py
@@ -1,7 +1,7 @@
 from django.shortcuts import redirect, HttpResponseRedirect
 from django.contrib.auth import logout
 from django.contrib.auth.models import User
-from shop.models import Profile, Item
+from shop.models import Profile, Item, Image
 from django import forms
 
 class UserForm(forms.ModelForm):
@@ -18,3 +18,8 @@ class ItemForm(forms.ModelForm):
 	class Meta:
 		model = Item
 		fields = ('name','price','description','category','location')
+
+class ImageForm(forms.ModelForm):
+	class Meta:
+		model = Image
+		fields = ('image',)
