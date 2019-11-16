e0ac58fcae8594770f8ab76f3d39ef73da73841e
Daniel <daniel.delrio@obf.ateneo.edu>
2019-11-16 21:44:05 +0800

    Add urls and views

--- ateneo_trade/shop/views.py
+++ ateneo_trade/shop/views.py
@@ -34,6 +34,9 @@ def update_profile(request):
 		'profile_form': profile_form
 	})
 
+def post_item(request):
+	return render(request, 'shop/post_item.html')
+
 def Logout(request):
 	logout(request)
 	return HttpResponseRedirect('/')
