9008d0f0be5de4f51eaf69b89dd4a378557ace29
Riana Lim <limriana9@gmail.com>
2019-11-26 11:20:53 +0800

    added view and path for view your items

--- ateneo_trade/shop/urls.py
+++ ateneo_trade/shop/urls.py
@@ -7,6 +7,7 @@ from . import views
 urlpatterns = [
     path('', login_required(views.Home.as_view()), name='home'),
     path('profile/', views.update_profile, name='profile'),
+    path('your-items/', login_required(views.ViewYourItems.as_view()), name='your-items'),
     path('post/', views.post_item, name='post_item'),
     path('account/logout/', views.Logout, name='logout'),
     path('item/<int:id>/detail/', login_required(views.ViewItemDetail.as_view()), name='view-item-detail'),
